3c5934ac848606599981754d13cf7d29f65d2815
m.Ammad <ammadking12345@gmail.com>
2024-03-23 12:17:11 +0500

    config index.js and npm i react-router-dom , syncfusion/ej2-base and added  sidebar context api

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import { CounterProvider } from './context/sidebar';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2UFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTX5QdEViWX5ccn1dRWZU');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CounterProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </CounterProvider>
   </React.StrictMode>
 );
 
